09cd4da0aef6c20651f6e1ffc880b046c4045bb6
Kolapo Adedipe <kolapoadedipe36@gmail.com>
2023-10-10 10:15:38 +0300

    Task0

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -6,25 +6,23 @@ needed in a CopyAll - Paste task
 
 
 def minOperations(n):
-    """
-    Method for computing the minimum number
-    of operations for task Copy All and Paste
-    Args:
-        n: input value
-    Return: the sum of the operations
-    """
-    if n < 2:
-        return 0
-    factor_sum = 0
-    i = 2  # Start with the first prime number
-    while n > 1:
-        if n % i == 0:
-            factor_sum += i
-            n //= i  # Use integer division
-        else:
-            i += 1
-    return factor_sum
+    if n == 1:
+        return 0  # Already have 1 H character
 
-# Example usage
-result = minOperations(12)
-print("Minimum operations:", result)
+    operations = [0] * (n + 1)
+
+    for i in range(2, n + 1):
+        operations[i] = float('inf')
+        for j in range(1, i):
+            if i % j == 0:
+                operations[i] = min(operations[i], operations[j] + i // j)
+
+    if operations[n] == float('inf'):
+        return 0  # It's impossible to achieve n H characters
+    else:
+        return operations[n]
+
+# Example usage:
+n = 9
+result = minOperations(n)
+print(f"Number of operations to achieve {n} H characters: {result}")
